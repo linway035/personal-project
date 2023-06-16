bdf9cb7bbaa94b24c456422b1d548478577a685b
Wayne <wayneck0138@gmail.com>
2023-06-16 17:30:34 +0800

    feat: add tweet_images model

---
+++ models/tweet_images.js
@@ -0,0 +1,29 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class tweet_images extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  tweet_images.init(
+    {
+      tweet_id: DataTypes.INTEGER,
+      filename: DataTypes.STRING,
+      image_path: DataTypes.STRING,
+      size: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'tweet_images',
+      tableName: 'tweet_images',
+      underscored: true,
+    }
+  )
+  return tweet_images
+}
