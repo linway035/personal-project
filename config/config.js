77a1b0f34841f15ec338d148fe4f135b3d5f4e8f
Wayne <wayneck0138@gmail.com>
2023-06-16 14:27:06 +0800

    feat: add mysql connection

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config() //原本為json，更改為js檔案
+
+module.exports = {
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+}
