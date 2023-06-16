8cc9682696162987ba046506707c1dd9c0516af2
Wayne <wayneck0138@gmail.com>
2023-06-16 15:50:45 +0800

    feat: add followships model

---
+++ migrations/20230616074148-create-followships.cjs
@@ -0,0 +1,37 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('followships', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      follower_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      following_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      is_active: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('followships')
+  },
+}
