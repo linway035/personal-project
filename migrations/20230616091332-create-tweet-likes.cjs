6fd0afd5af14d874cfd89809a6cfb19378121b14
Wayne <wayneck0138@gmail.com>
2023-06-16 17:15:06 +0800

    feat: add tweet-likes model

---
+++ migrations/20230616091332-create-tweet-likes.cjs
@@ -0,0 +1,38 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tweet_likes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      tweet_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      is_active: {
+        allowNull: false,
+        defaultValue: 1,
+        type: Sequelize.BOOLEAN,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tweet_likes')
+  },
+}
---
+++ models/tweet_likes.js
@@ -0,0 +1,28 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class tweet_likes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  tweet_likes.init(
+    {
+      user_id: DataTypes.INTEGER,
+      tweet_id: DataTypes.INTEGER,
+      is_active: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: 'tweet_likes',
+      tableName: 'tweet_likes',
+      underscored: true,
+    }
+  )
+  return tweet_likes
+}
