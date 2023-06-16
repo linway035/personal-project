434f017a0b3a0922e565f25110b8141db35d8326
Wayne <wayneck0138@gmail.com>
2023-06-16 15:01:10 +0800

    feat: add users model

---
+++ migrations/20230616065557-create-users.js
@@ -0,0 +1,54 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      provider: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      avatar: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      cover: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      bio: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users')
+  },
+}
