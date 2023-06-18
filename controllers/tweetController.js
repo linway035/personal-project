0be6724c23f9caad97171df87c681fffb41175f2
Wayne <wayneck0138@gmail.com>
2023-06-18 14:00:19 +0800

    feat: add hidden tweet query

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -5,9 +5,12 @@ const tweetController = {
     SELECT tweets.*, users.name, users.avatar
     FROM tweets
     JOIN users ON tweets.user_id = users.id
+    WHERE tweets.id NOT IN (
+      SELECT tweet_id FROM hidden_tweets WHERE user_id = ${res.locals.userId}
+    )
     ORDER BY tweets.updated_at DESC;
     `)
-    // console.log(data)
+    console.log(data.length)
     console.log(res.locals.userId)
     res.render('tweets', { tweets: data })
   },
